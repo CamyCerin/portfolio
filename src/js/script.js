29186b0b41b66c4ba90fc55d11960a3cdd26d07c
Camille Cerin <camille.cerin@gmail.com>
2020-02-13 12:09:57 +0100

    test

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// CODE MAEVOOOO
+//   var menuBurger = document.getElementById("nav");
+// var menuOpen = document.getElementById("menuOpen");
+// var closeMenu = document.getElementById("closeMenu");
+
+// menuBurger.addEventListener("click", function() {
+//   menuOpen.classList.toggle("isOpen");
+// });
+
+// closeMenu.addEventListener("click", function() {
+//   menuOpen.classList.remove("isOpen");
+// });
+console.log("hellow");
